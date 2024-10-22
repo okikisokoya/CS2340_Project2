eef495244664924f0f94cfbdf0626134913ffa54
Xander Riddle <xriddle0917@gmail.com>
2024-10-22 13:23:18 -0400

    restructured project and reconfigured to add Django compatibility

--- CS2340_Project2/asgi.py
+++ CS2340_Project2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for spotify_webapp project.
+ASGI config for CS2340_Project2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CS2340_Project2.settings')
 
 application = get_asgi_application()
--- CS2340_Project2/wsgi.py
+++ CS2340_Project2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for spotify_webapp project.
+WSGI config for CS2340_Project2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CS2340_Project2.settings')
 
 application = get_wsgi_application()
