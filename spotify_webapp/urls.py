4b09760ee44a76273a9137b1817a18e5545f3030
Joanne Park <68297752+jp135@users.noreply.github.com>
2024-11-05 14:29:36 -0500

    login/create user/delete user functionality

--- spotify_webapp/urls.py
+++ spotify_webapp/urls.py
@@ -5,7 +5,16 @@ from spotify_webapp import views  # Changed to absolute import
 app_name = 'spotify_webapp'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    #our webapp
+path('', views.home, name='home'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('delete-account/', views.delete_account, name='delete_account'),
+
+    #for spotify
+    path('login/', views.spotify_login_view, name='login'),
     path('callback/', views.callback, name='callback'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('top-tracks/', views.get_top_tracks, name='top-tracks'),
