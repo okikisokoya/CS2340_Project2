1ef200ad07c83a12e2d82bb8c434b935b576dcf5
Jinhee <jinheechang0329@gmail.com>
2024-11-12 14:24:54 -0500

    starting the code for clicky game (yay!)

--- wrapped/src/Wrapped Game/clickygame.py
+++ wrapped/src/Wrapped Game/clickygame.py
@@ -2,26 +2,42 @@
 #user uses W, player uses the up arrow key
 #whoever wins get +2 points (yippee!)
 
-import turtle
+# import turtle
+#
+# clickgame = turtle.Screen()
+# clickgame.title("Battle of the Jedi and the Sith!")
+# clickgame.bgcolor('black')
+#
+# clickgame.register_shape("robby.png")
+# robby = turtle.Turtle()
+# robby.shape("robby.png")
+# robby.speed(0)
+#
+# jediClicks = 0;
+# sithClicks = 0;
+#
+# pen = turtle.Turtle()
+# pen.hideturtle()
+# pen.color('white')
+# pen.penup()
+# pen.goto(0,400) #xy coordinates
+# pen.write(f"Clicks: {jediClicks}", align = "center", font = ("Arial", 20, "normal"))
+#
+# clickgame.mainloop()
 
-clickgame = turtle.Screen()
-clickgame.title("Battle of the Jedi and the Sith!")
-clickgame.bgcolor('black')
+import tkinter as tk
+from tkinter import messagebox
+import threading
+import time
 
-clickgame.register_shape("robby.png")
-robby = turtle.Turtle()
-robby.shape("robby.png")
-robby.speed(0)
+window = tk.Tk()
+window.title("Jedi vs Sith: Duel of the Force!")
 
-jediClicks = 0;
-sithClicks = 0;
+jedi_score = 0
+sith_score = 0
 
-pen = turtle.Turtle()
-pen.hideturtle()
-pen.color('white')
-pen.penup()
-pen.goto(0,400) #xy coordinates
-pen.write(f"Clicks: {jediClicks}", align = "center", font = ("Arial", 20, "normal"))
-
-clickgame.mainloop()
+def jediClick():
+  global jedi_score
+  jedi_score += 1
+  jedi_label.config(text = f"Jedi SCore: {jedi_score}")
 
