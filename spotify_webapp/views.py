616db9b3484b885dd7f5f7181a6fc25fc95d0ce7
Jinhee <jinheechang0329@gmail.com>
2024-11-14 20:29:25 -0500

    starting the code for clicky game (yay!)

--- spotify_webapp/views.py
+++ spotify_webapp/views.py
@@ -2,18 +2,24 @@
 from django.shortcuts import render, redirect
 import urllib
 import requests
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import redirect
 from datetime import datetime, timedelta
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from spotipy import SpotifyOAuth
+
 from .models import User
 from django.http import JsonResponse
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_protect
 
+from CS2340_Project2.secret import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 
 def spotify_login_view(request):
     scope = 'user-read-private user-top-read'
@@ -54,15 +60,36 @@ def callback(request):
 
 
 def get_top_tracks(request):
-    if 'access_token' not in request.session:
-        return redirect('spotify_webapp:login')
+    sp_oauth = SpotifyOAuth(
+        client_id= SPOTIFY_CLIENT_ID,
+        client_secret= SPOTIFY_CLIENT_SECRET,
+        redirect_uri= settings.SPOTIFY_REDIRECT_URI,
+        scope=settings.SPOTIFY_SCOPES,
+    )
+    if request.GET.get("code"):
+        token_info = sp_oauth.get_access_token(request.GET["code"])
+        sp = spotipy.Spotify(auth=token_info['access_token'])
+    topTracks = sp.current_user_top_tracks(limit=5, offset=0,time_range='medium_term')
+    #printing to terminal??
+    top_tracks = []
+    for idx, track in enumerate(topTracks['items']):
+        track_name = track['name']
+        artist_name = track['artists'][0]['name']
+        top_tracks.append(f"{idx + 1}. {track_name} by {artist_name}")
+        print(f"{idx + 1}. {track_name} by {artist_name}")
 
-    headers = {
-        'Authorization': f"Bearer {request.session['access_token']}"
-    }
+    return HttpResponse("<br>".join(top_tracks))
 
-    response = requests.get('https://api.spotify.com/v1/me/top/tracks', headers=headers)
-    return JsonResponse(response.json())
+        #importing spotipy
+    # if 'access_token' not in request.session:
+    #     return redirect('spotify_webapp:login')
+    #
+    # headers = {
+    #     'Authorization': f"Bearer {request.session['access_token']}"
+    # }
+    #
+    # response = requests.get('https://api.spotify.com/v1/me/top/tracks', headers=headers)
+    # return JsonResponse(response.json())
 
 
 def refresh_token(request):
