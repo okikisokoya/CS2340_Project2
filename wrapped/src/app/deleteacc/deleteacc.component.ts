999ca22e4b9becb6d26c9c0a1f76fc2abd12f714
Xander Riddle <xriddle0917@gmail.com>
2024-11-19 13:30:26 -0500

    added some details to existing pages, also links didn't work for me so I reimplemented them with <a> tag

--- wrapped/src/app/deleteacc/deleteacc.component.ts
+++ wrapped/src/app/deleteacc/deleteacc.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';  
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-deleteacc',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './deleteacc.component.html',
   styleUrl: './deleteacc.component.css'
 })
