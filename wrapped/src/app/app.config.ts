d3b00fa818d102482b49e767046f020dff31038d
Olivia Li <oli34@gatech.edu>
2024-11-28 06:49:28 -0500

    make spotify.service.ts

--- wrapped/src/app/app.config.ts
+++ wrapped/src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';  // For making HTTP requests
+import { provideHttpClient, withFetch } from '@angular/common/http';  // For making HTTP requests
 import { FormsModule } from '@angular/forms';  // For ngModel and two-way data binding
 import { routes } from './app.routes';  // Your app's routes
 import { provideClientHydration } from '@angular/platform-browser';  // For client-side hydration
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),  // For HTTP requests
     importProvidersFrom(FormsModule),  // For ngModel two-way binding
     importProvidersFrom(RouterModule.forRoot(routes)),
     provideClientHydration(),  // Enable hydration for browser rendering
--- wrapped/src/app/top-songs/top-songs.component.ts
+++ wrapped/src/app/top-songs/top-songs.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-top-songs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './top-songs.component.html',
   styleUrl: './top-songs.component.css'
 })
